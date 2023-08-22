3b4ac7eb8b6d9e180aa010d60d26724cc6b2d31e
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-22 17:22:01 -0400

    Add google maps api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+/// <reference types="@types/google.maps" />
 import { User } from './user';
+import { Company } from './Company';
 
 const user = new User();
-console.log(user);
+const company = new Company();
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
